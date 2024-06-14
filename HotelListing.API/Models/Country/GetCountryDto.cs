83318480ebc37640460bb0621ee55d519fa8f509
clyvissianos <c.lyvissianos@gmail.com>
2024-06-14 20:30:28 +0300

    REST API Development Best Practices. Automapper and repositories

---
+++ HotelListing.API/Models/Country/GetCountryDto.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListing.API.Models.Country
+{
+    public class GetCountryDto : BaseCountryDto
+    {   
+    }
+}
--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,4 +1,7 @@
+using HotelListing.API.Configurations;
+using HotelListing.API.Contracts;
 using HotelListing.API.Data;
+using HotelListing.API.Repository;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -25,6 +28,11 @@ builder.Services.AddCors(options =>
 });
 
 builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 
 var app = builder.Build();
 
